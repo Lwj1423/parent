17e934a9bd3a67b2205b4669d8813288c75bfd91
Lwj1423 <m17621954542@163.com>
2021-06-04 11:33:26 +0800

    查找算法

---
+++ dateStruct/src/main/java/search/SeqSearch.java
@@ -0,0 +1,35 @@
+package search;
+
+/**
+ *
+ * 线性查找
+ */
+public class SeqSearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5,3};
+        int index = seqSearch(arr,3);
+        if (index == -1){
+            System.out.println("没招到");
+        }else{
+            System.out.println("找打，下标为=：" + index);
+        }
+    }
+
+    /**
+     *  线性查找一个满足条件的值，就返回
+     * @param arr
+     * @param i
+     * @return
+     */
+    private static int seqSearch(int[] arr, int i) {
+        //线性查找，逐一对比
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] == i){
+                return j;
+            }
+        }
+
+        return -1;
+    }
+}
