961ebe2520728712026878905dc7bd5543f71b5d
Lwj1423 <m17621954542@163.com>
2021-06-04 10:54:26 +0800

    数据结构和算法

--- dateStruct/src/test/java/test01.java
+++ dateStruct/src/test/java/test01.java
@@ -3,14 +3,38 @@ import org.junit.Test;
 public class test01 {
 
     @Test
-    public void test01(){
+    public void test01() {
         int[][] arr = new int[2][4];
         System.out.println(arr.length);
 
-        int a = 10/3;
+        int a = 10 / 3;
         System.out.println(a);
 
-        int b = 10%3;
+        int b = 10 % 3;
         System.out.println(b);
+
+
+        System.out.println("循环");
+    }
+
+
+
+    @Test
+    public void test02() {
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (j == 2) {
+                    test03();
+                }
+                System.out.println("内部"+ j);
+            }
+            System.out.println("外部"+ i);
+
+        }
+    }
+
+    public void test03(){
+        return;
     }
 }
